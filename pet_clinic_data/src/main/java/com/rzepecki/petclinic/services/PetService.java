65147603d4888cf0e00f3d5618779fef42e631f6
marcinrzepeckiwroc <marcin_rzepecki@interia.pl>
2020-01-03 09:31:13 +0100

    refactor services interface (CrudService)

--- pet_clinic_data/src/main/java/com/rzepecki/petclinic/services/PetService.java
+++ pet_clinic_data/src/main/java/com/rzepecki/petclinic/services/PetService.java
@@ -1,13 +1,8 @@
 package com.rzepecki.petclinic.services;
 
-import com.rzepecki.petclinic.model.Owner;
 import com.rzepecki.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
 
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
--- pet_clinic_data/src/main/java/com/rzepecki/petclinic/services/VetService.java
+++ pet_clinic_data/src/main/java/com/rzepecki/petclinic/services/VetService.java
@@ -1,13 +1,8 @@
 package com.rzepecki.petclinic.services;
 
-import com.rzepecki.petclinic.model.Pet;
 import com.rzepecki.petclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long>{
 
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
