91c84eb5a6a02584b2bdc719a1364a84d18bde34
marcinrzepeckiwroc <marcin_rzepecki@interia.pl>
2020-01-15 15:39:06 +0100

    lombok inital

--- pet_clinic_data/src/main/java/com/rzepecki/petclinic/services/OwnerService.java
+++ pet_clinic_data/src/main/java/com/rzepecki/petclinic/services/OwnerService.java
@@ -1,7 +1,7 @@
 package com.rzepecki.petclinic.services;
 
 import com.rzepecki.petclinic.model.Owner;
-
+import org.springframework.stereotype.Service;
 
 public interface OwnerService extends CrudService<Owner, Long>{
 
